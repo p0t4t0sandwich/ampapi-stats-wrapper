91c4d7cfa2b8583b6dc2d230a6aee7d06fdda979
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-09-30 04:46:23 -0600

    Initial implementation finished, just needs docs

---
+++ main.go
@@ -0,0 +1,351 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/p0t4t0sandwich/ampapi-go"
+	"github.com/p0t4t0sandwich/ampapi-go/modules"
+)
+
+// -------------- Global Variables --------------
+
+// Server manager
+var serverManager ServerManager = ServerManager{}
+
+func main() {
+	// Get IP from env
+	ip := os.Getenv("IP_ADDRESS")
+	if ip == "" {
+		ip = "0.0.0.0"
+	}
+
+	// Get port from env
+	port := os.Getenv("REST_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	serverManager = ServerManager{
+		controller: *modules.NewADS(
+			os.Getenv("AMP_API_URL"),
+			os.Getenv("AMP_API_USERNAME"),
+			os.Getenv("AMP_API_PASSWORD"),
+		),
+		targetData:   make(map[string]Data[ampapi.IADSInstance, modules.ADS]),
+		instanceData: make(map[string]Data[ampapi.Instance, interface{}]),
+	}
+	serverManager.InitInstnaceData()
+	fmt.Println("Initialized instance data")
+
+	// Initialize/Update the server manager
+	go func() {
+		for {
+			time.Sleep(5 * time.Minute)
+			serverManager.InitInstnaceData()
+			fmt.Println("Updated instance data")
+		}
+	}()
+
+	gin.SetMode(gin.ReleaseMode)
+	var router *gin.Engine = gin.Default()
+
+	// API routes
+	router.GET("/", getRoot)
+	router.GET("/target/status/:targetName", getTargetStatus)
+	router.GET("/instance/status/simple/:instanceName", getInstanceStatusSimple)
+	router.GET("/server/status/:serverName", getServerStatus)
+	router.GET("/server/status/simple/:serverName", getServerStatusSimple)
+
+	router.Run(ip + ":" + port)
+}
+
+// -------------- Structs --------------
+
+// Data - Generic data struct
+type Data[T any, R any] struct {
+	Data T
+	API  R
+}
+
+// -------------- Methods --------------
+
+// Instance handler
+type ServerManager struct {
+	controller   modules.ADS
+	targetData   map[string]Data[ampapi.IADSInstance, modules.ADS]
+	instanceData map[string]Data[ampapi.Instance, interface{}]
+}
+
+// InitInstnaceData - Initialize the instance data
+func (s *ServerManager) InitInstnaceData() {
+	// Get all instances
+	var targets []ampapi.IADSInstance = s.controller.ADSModule.GetInstances().Result
+
+	for i := 0; i <= len(targets)-1; i++ {
+		var instances []ampapi.Instance = targets[i].AvailableInstances
+
+		// Check if the target exists
+		if s.TargetExists(targets[i].FriendlyName) {
+			// Update the target data
+			s.targetData[targets[i].FriendlyName] = Data[ampapi.IADSInstance, modules.ADS]{
+				Data: targets[i],
+				API:  s.targetData[targets[i].FriendlyName].API,
+			}
+		} else {
+			// Add the target data
+			s.targetData[targets[i].FriendlyName] = Data[ampapi.IADSInstance, modules.ADS]{
+				Data: targets[i],
+				API:  modules.ADS{},
+			}
+		}
+
+		for j := 0; j <= len(instances)-1; j++ {
+			// Check if the instance exists
+			if s.InstanceExists(instances[j].FriendlyName) {
+				// Update the instance data
+				s.instanceData[instances[j].FriendlyName] = Data[ampapi.Instance, interface{}]{
+					Data: instances[j],
+					API:  s.instanceData[instances[j].FriendlyName].API,
+				}
+			} else {
+				// Add the instance data
+				s.instanceData[instances[j].FriendlyName] = Data[ampapi.Instance, interface{}]{
+					Data: instances[j],
+					API:  nil,
+				}
+			}
+		}
+	}
+
+	// Remove targets that are not present in targets[i]
+	for i := 0; i <= len(s.targetData)-1; i++ {
+		var found bool = false
+		for j := 0; j <= len(targets)-1; j++ {
+			if s.targetData[targets[j].FriendlyName].Data.FriendlyName == targets[j].FriendlyName {
+				found = true
+				break
+			}
+		}
+		if !found {
+			delete(s.targetData, s.targetData[targets[i].FriendlyName].Data.FriendlyName)
+		}
+
+		var instances []ampapi.Instance = targets[i].AvailableInstances
+		// Remove instances that are not present in instances[j]
+		for j := 0; j <= len(s.instanceData)-1; j++ {
+			var found bool = false
+			for k := 0; k <= len(instances)-1; k++ {
+				if s.instanceData[instances[k].FriendlyName].Data.FriendlyName == instances[k].FriendlyName {
+					found = true
+					break
+				}
+			}
+			if !found {
+				delete(s.instanceData, s.instanceData[instances[j].FriendlyName].Data.FriendlyName)
+			}
+		}
+	}
+}
+
+// InitTargetAPI - Initialize a target's API
+func (s *ServerManager) InitTargetAPI(targetName string) {
+	// Get the target data
+	var targetData Data[ampapi.IADSInstance, modules.ADS] = s.targetData[targetName]
+
+	// Initialize the target's API
+	var api interface{} = s.controller.InstanceLogin(targetData.Data.InstanceId, "ADS")
+	if api == nil {
+		return
+	}
+	targetData.API = *api.(*modules.ADS)
+
+	// Store the target data
+	s.targetData[targetName] = targetData
+}
+
+// InitInstanceAPI - Initialize an instance's API
+func (s *ServerManager) InitInstanceAPI(instanceName string) {
+	// Get the instance data
+	var instanceData Data[ampapi.Instance, interface{}] = s.instanceData[instanceName]
+
+	// Initialize the instance's API
+	instanceData.API = s.controller.InstanceLogin(instanceData.Data.InstanceID, instanceData.Data.Module)
+
+	// Store the instance data
+	s.instanceData[instanceName] = instanceData
+}
+
+// TargetExists - Check if a target exists
+func (s *ServerManager) TargetExists(targetName string) bool {
+	_, ok := s.targetData[targetName]
+	return ok || targetName == "ADS01"
+}
+
+// InstanceExists - Check if an instance exists
+func (s *ServerManager) InstanceExists(instanceName string) bool {
+	_, ok := s.instanceData[instanceName]
+	return ok || instanceName == "ADS01"
+}
+
+// GetTargetAPI - Get a target's API
+func (s *ServerManager) GetTargetAPI(targetName string) modules.ADS {
+	if targetName == "ADS01" {
+		return s.controller
+	}
+	if s.targetData[targetName].API.Username == "" {
+		s.InitTargetAPI(targetName)
+	}
+	return s.targetData[targetName].API
+}
+
+// GetTargetData - Get a target's data
+func (s *ServerManager) GetTargetData(targetName string) ampapi.IADSInstance {
+	return s.targetData[targetName].Data
+}
+
+// GetInstanceAPI - Get an instance's API
+func (s *ServerManager) GetInstanceAPI(instanceName string) interface{} {
+	if instanceName == "ADS01" {
+		return s.controller
+	}
+	if s.instanceData[instanceName].API == nil {
+		s.InitInstanceAPI(instanceName)
+	}
+	return s.instanceData[instanceName].API
+}
+
+// GetInstanceData - Get an instance's data
+func (s *ServerManager) GetInstanceData(instanceName string) ampapi.Instance {
+	return s.instanceData[instanceName].Data
+}
+
+// -------------- Functions --------------
+
+// targetStatus - Get a target's status
+func targetStatus(targetName string) ampapi.Status {
+	return serverManager.GetTargetAPI(targetName).Core.GetStatus()
+}
+
+// instanceStatusSimple - Get the simple status of an instance
+func instanceStatusSimple(instanceName string) string {
+	for targetName := range serverManager.targetData {
+		for _, instance := range serverManager.targetData[targetName].Data.AvailableInstances {
+			if instance.FriendlyName == instanceName {
+				instaceId := instance.InstanceID
+				API := serverManager.GetTargetAPI(targetName)
+				instanceStatuses := API.ADSModule.GetInstanceStatuses().Result
+				for i := 0; i <= len(instanceStatuses)-1; i++ {
+					if instanceStatuses[i].InstanceID == instaceId {
+						running := instanceStatuses[i].Running
+						if running {
+							return "Running"
+						} else {
+							return "Offline"
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return "Not implemented"
+}
+
+// serverStatus - Get a server's status
+func serverStatus(serverName string) ampapi.Status {
+	var instanceData ampapi.Instance = serverManager.GetInstanceData(serverName)
+	switch instanceData.Module {
+	case "ADS":
+		return serverManager.GetTargetAPI(instanceData.FriendlyName).Core.GetStatus()
+	case "Minecraft":
+		return serverManager.GetInstanceAPI(instanceData.FriendlyName).(*modules.Minecraft).Core.GetStatus()
+	case "GenericModule":
+		return serverManager.GetInstanceAPI(instanceData.FriendlyName).(*modules.GenericModule).Core.GetStatus()
+	default:
+		return serverManager.GetInstanceAPI(instanceData.FriendlyName).(*modules.CommonAPI).Core.GetStatus()
+	}
+}
+
+// serverStatusSimple - Get the simple status of a server
+func serverStatusSimple(serverName string) string {
+	return serverStatus(serverName).State.String()
+}
+
+// -------------- Handlers --------------
+
+// Get root route
+func getRoot(c *gin.Context) {
+	// Read the html file
+	html, err := os.ReadFile("index.html")
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Return the html
+	c.Data(http.StatusOK, "text/html", html)
+}
+
+// Get target status
+func getTargetStatus(c *gin.Context) {
+	// Get the target name
+	targetName := c.Param("targetName")
+
+	// Check if the target exists
+	if !serverManager.TargetExists(targetName) {
+		c.String(http.StatusNotFound, "Target not found")
+		return
+	}
+
+	// Return the status
+	c.JSON(http.StatusOK, targetStatus(targetName))
+}
+
+// Get instance status simple
+func getInstanceStatusSimple(c *gin.Context) {
+	// Get the instance name
+	instanceName := c.Param("instanceName")
+
+	// Check if the instance exists
+	if !serverManager.InstanceExists(instanceName) {
+		c.String(http.StatusNotFound, "Instance not found")
+		return
+	}
+
+	// Return the status
+	c.String(http.StatusOK, instanceStatusSimple(instanceName))
+}
+
+// Get server status
+func getServerStatus(c *gin.Context) {
+	// Get the server name
+	serverName := c.Param("serverName")
+
+	// Check if the server exists
+	if !serverManager.InstanceExists(serverName) {
+		c.String(http.StatusNotFound, "Server not found")
+		return
+	}
+
+	// Return the status
+	c.JSON(http.StatusOK, serverStatus(serverName))
+}
+
+// Get server status simple
+func getServerStatusSimple(c *gin.Context) {
+	// Get the server name
+	serverName := c.Param("serverName")
+
+	// Check if the server exists
+	if !serverManager.InstanceExists(serverName) {
+		c.String(http.StatusNotFound, "Server not found")
+		return
+	}
+
+	// Return the status
+	c.String(http.StatusOK, serverStatusSimple(serverName))
+}
